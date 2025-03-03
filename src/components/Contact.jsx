e7b8b1ad56cf9fe052fc0624050298e93ed970b7
TosinJoseph <tosinjosephoffice2@gmail.com>
2025-03-03 14:56:21 -0800

    added animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const Contact = () => {
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden ' id="Contact">
+    <motion.div 
+    initial={{opacity: 0, x: -200}}
+        transition={{duration: 1}}
+        whileInView={{opacity: 1, x: 0}}
+        viewport={{once: true}}
+    className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden ' id="Contact">
      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact
              <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
         <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Ready to Make Move? Lets Build Your Future Together.</p>
@@ -27,7 +33,7 @@ const Contact = () => {
             </div>
             <button className='rounded bg-blue-600 text-white py-2 px-12 mb-10 hover:bg-blue-200'>Send Message</button>
         </form>
-    </div>
+    </motion.div>
   )
 }
 
